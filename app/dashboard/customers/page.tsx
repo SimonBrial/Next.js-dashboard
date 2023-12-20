d18e1196ce603270b2df3f750d64de2fbdbf05c2
SimonBrial <simon.antonio.ba18@gmail.com>
2023-12-20 12:06:21 -0400

    Segundo commit

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,5 @@
+function page() {
+  return <p>Dashboard/ Customers Page</p>;
+}
+
+export default page;
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,5 @@
+function page() {
+  return <p>Dashboard/invoices Page</p>;
+}
+
+export default page;
